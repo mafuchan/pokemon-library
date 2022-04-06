81eaf5c4b350bf170a6060797c5e9d240cc6c61f
Matt Crecraft <mcrecraft@gmail.com>
2022-04-06 13:53:52 -0500

    Pokemon on the screen

--- site/index.js
+++ site/index.js
@@ -1,4 +1,26 @@
 const url = "https://pokeapi.co/api/v2/pokemon?limit=50"
+const app = document.querySelector("#app")
 const main = document.querySelector("main")
 const spinner = document.querySelector(".spinner")
 
+function addPokemonImage(url) {
+    const img = document.createElement("img")
+    img.src = url
+    app.append(img)
+}
+
+fetch(url)
+    .then(response => {
+        return response.json()
+    }).then(parsedResponse => {
+        const urls = parsedResponse.results.map(result => result.url)
+        const fetches = urls.map(url => fetch(url).then(response => response.json()))
+        return Promise.all(fetches)
+    }).then(responses => {
+        spinner.classList.add("hidden")
+        responses.forEach(response => {
+            addPokemonImage(response.sprites.front_default)
+        })
+    })
+
+document.querySelector("h1").textContent = "Pokedex!"
